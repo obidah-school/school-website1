cee6a5eef839d9b9a872389e27855ba3e96f2e5f
obidah-school <hlahik200@gmail.com>
2026-03-19 00:36:59 +0300

    Update and rename api/api/send-sms.cjs to api/api/api/send-sms.js

--- api/api/api/send-sms.js
+++ api/api/api/send-sms.js
@@ -1,4 +1,3 @@
-// api/send-sms.cjs
 module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
